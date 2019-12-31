fa3566da1a9d6edf6cf20fe4551a6cc901cbf2dc
etr1hc <Thong.LeTrung@vn.bosch.com>
2019-12-31 05:58:16 +0100

    store data in file

--- 07.file/main.cpp
+++ 07.file/main.cpp
@@ -28,17 +28,6 @@ using namespace std;
 class FileSystem{
   public:
      FileSystem(){
-	   // _myFile.open("test.txt",   ios::out | ios::in | ios::trunc);
-		
-		bool existFile = false;
-
-		if(existFile == true){
-			 _myFile.open("test.txt",   ios::out | ios::in | ios::binary);
-		}
-		else{
-			 _myFile.open("test.txt",   ios::out | ios::in | ios::trunc);
-		}
-		
 		loadMap();
 	 }
 
@@ -90,7 +79,7 @@ class FileSystem{
 
 	    if(true == hasIndex){
 			readfile( (POSITION_BODY_START + index*SIZE_MAX_WORD), value);
-			std::cout << "searchKey value=" << value <<endl;
+			std::cout <<"index="<< index << " searchKey value=" << value <<endl;
 
 		}else{
 			 value = "";	 
@@ -99,9 +88,22 @@ class FileSystem{
 	 
 	 
 	 void readfile(int index, std::string& text){
+
+		 bool existFile = hasFile("test.txt");
+		fstream _myFile;
+
+		if(existFile == true){
+			 _myFile.open("test.txt",   ios::out | ios::in );
+		}
+		else{
+			 _myFile.open("test.txt",   ios::out | ios::in | ios::trunc);
+		}
+		
 		 char buffer[SIZE_MAX_WORD] ={}; 
 		 if (_myFile.is_open() ){
-			 _myFile.seekg(index,ios::beg);
+			// _myFile.seekg(index,_myFile.beg);
+			_myFile.flush();
+			 _myFile.seekg(index, ios::beg);
 			 _myFile.read(buffer, SIZE_MAX_WORD -1); 
 		 }else{
 			  std::cout << "read  Error open";
@@ -111,54 +113,94 @@ class FileSystem{
 		text = std::string(buffer);
 
 		 std::cout << "readfile  index =" << index  <<" buffer="<<buffer<<endl;
+
+		 if(_myFile.is_open()){
+			  _myFile.close(); 
+		 }
 	 }
 	 
 	 void writefile(int index, const std::string& text){
+		bool existFile = hasFile("test.txt");
+		fstream _myFile;
+
+		if(existFile == true){
+			 _myFile.open("test.txt",   ios::out | ios::in );
+		}
+		else{
+			 _myFile.open("test.txt",   ios::out | ios::in | ios::trunc);
+		}
+
 		 std::cout << "writefile  index=" << index  <<" text="<<text <<" len="<< text.length()<<endl;
 		 if(_myFile.is_open()){
-			  _myFile.seekg(index, ios::beg);
+			  _myFile.seekg(index );
 		 	  _myFile.write (text.c_str(), text.length());  
 		 }else{
 			  std::cout << "write  Error open";
 		 }
-		
-	 }
-	 
-	 ~FileSystem(){
+
 		 if(_myFile.is_open()){
 			  _myFile.close(); 
 		 }
 		
 	 }
-	 
-  private:
-	void loadMap(){
-	    char buffer[POSITION_BODY_START] ={};  
+
+	 std::string getHeader(){
+
+		 bool existFile = hasFile("test.txt");
+		fstream _myFile;
+
+		if(existFile == true){
+			 _myFile.open("test.txt",   ios::out | ios::in );
+		}
+		else{
+			 _myFile.open("test.txt",   ios::out | ios::in | ios::trunc);
+		}
+
+		std::string  header ="";
+		char buffer[POSITION_BODY_START] ={};  
 		if (_myFile.is_open() ){
 			if(_myFile.tellg() > 0){
-			     _myFile.seekg(0,ios::beg);
+				_myFile.seekg(0,ios::beg);
 				_myFile.read(buffer, POSITION_BODY_START -1); 
 			}
 
-		
-
-			 int len;
-			int index;
-			std::string key;
-
-			std::string temp;
-			std::string  header = std::string(buffer);
-
-			for(int i = 0; i < header.length(); i++ ){
-				temp = header.substr(0, SIZE_OF_LEN); 
-
-				len = int2String(temp);
+			
+		 header = std::string(buffer);
+			if(_myFile.is_open()){
+				_myFile.close(); 
 			}
+		}
+		return header;
+	 }
+	 
+	 ~FileSystem(){
+		 
+	 }
+	 
+  private:
 
-
-		 }else{
-			  std::cout << "loadMap  Error open";
+    bool hasFile(const std::string& path){
+		bool ret =false;
+		 if (FILE *file = fopen(path.c_str(), "r")) {
+        	fclose(file);
+			ret = true;
 		 }
+		return ret;
+	}
+	
+	void loadMap(){
+		int len;
+		int index;
+		std::string key;
+
+		std::string temp;
+		std::string  header = getHeader();
+
+		for(int i = 0; i < header.length(); i++ ){
+			temp = header.substr(0, SIZE_OF_LEN); 
+
+			len = int2String(temp);
+		}
 	}
 
 	int int2String(const std::string& number){
@@ -175,7 +217,7 @@ class FileSystem{
 		char  key[20];
 	};
 
-	fstream _myFile;
+	
 
 	std::map<int, std::string> _hashTable;
 };
@@ -191,6 +233,10 @@ int main (int argc, char** argv) {
 	db.insertKey("abc", "1231111111111111111");
 
 	std::string value = "";
+
+	cout<<"\n search key"<<endl;
+	db.searchKey("123", value);
+	db.searchKey("abc", value);
 	db.searchKey("123", value);
 	return 0;
 }  
