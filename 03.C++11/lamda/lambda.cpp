213edbe941b85946e36541dc79f58b487506b46a
Thong LT <letrthong@gmail.com>
2021-10-10 00:37:07 +0700

    Create lambda.cpp

---
+++ 03.C++11/lamda/lambda.cpp
@@ -0,0 +1,26 @@
+//https://www.tutorialspoint.com/compile_cpp11_online.php
+
+#include <iostream>
+using namespace  std;
+
+
+void (*callback)(int); 
+
+void real_function(int x) {
+    cout<<"hello world x="<< x <<endl;
+    
+}
+
+int main()
+{
+     callback = &real_function;
+     callback(3);
+    
+    
+     callback  = [](int x){  
+         
+         cout<<"lambda hello world x="<< x<<endl;
+     };
+     
+     callback(3);
+}
