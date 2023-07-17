c0f2bfd5649c63d76467924ed5831de75aa71c12
Thong LT <letrthong@gmail.com>
2023-07-17 10:07:34 +0700

    Create share_object.cpp

---
+++ 03.C++11/share_object.cpp
@@ -0,0 +1,26 @@
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+struct base : public std::enable_shared_from_this<base>
+{
+    int val;
+    shared_ptr<base> share_more()
+    {
+        return shared_from_this();
+    }
+};
+
+int main()
+{
+    auto sp1 = make_shared<base>();
+    auto sp2 = sp1->share_more();
+
+    sp1->val = 3;
+    cout << "sp2->val == " << sp2->val << endl;
+    
+    sp2->val = 5;
+    cout << "sp1->val == " << sp1->val << endl;
+    return 0;
+}
