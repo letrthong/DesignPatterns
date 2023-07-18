09cf8373364fcc18bef783b160ff1dd97a9889a6
Thong LT <letrthong@gmail.com>
2023-07-18 15:10:49 +0700

    Update bind_abc.cpp

--- 03.C++11/bind_abc.cpp
+++ 03.C++11/bind_abc.cpp
@@ -1,13 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <iostream>
+
+using namespace std;
+
 // function which has to be binded.
 int add(int a, int b) { return a + b; }
-  
+ 
+ class  A{
+    public:
+     int sum(int a, int b) { 
+         cout <<"A::sum a=" << a <<" b= " << b << " -> " << a + b;
+         return a + b; 
+         
+     }
+     
+ };
 int main()
 {
     auto addf = bind(add, 10, 20);
     // binding the Function add() to values 10 and 20.
     cout << "The output is " << addf() << endl;
     // the Functor addf is called.
+    
+    A a;
+    
+    auto ptr_to_print_sum = std::mem_fn(&A::sum);
+    auto f4 = std::bind(ptr_to_print_sum, & a, 95,1);
+    
+    
+    
+    f4();
+    
     return 0;
 }
