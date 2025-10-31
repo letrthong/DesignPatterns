facdc2544824d78be98c35d45515d40705fbab07
Thong LT <letrthong@gmail.com>
2025-10-31 10:03:29 +0700

    Update atomic.cpp

--- 03.C++11/atomic.cpp
+++ 03.C++11/atomic.cpp
@@ -1,1 +1,12 @@
-https://en.cppreference.com/w/cpp/atomic/atomic
+//https://en.cppreference.com/w/cpp/atomic/atomic
+#include <atomic>
+#include <iostream>
+
+int main() {
+    std::atomic<int> counter(0);
+
+    counter++;  // atomic increment
+    std::cout << "Counter: " << counter.load() << std::endl;
+
+    return 0;
+}
